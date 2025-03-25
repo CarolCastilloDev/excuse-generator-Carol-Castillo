27ce2059b98d6e2a62de4b699807c9dc68314f67
Carolina Castillo Lucas <carolina_c.l@hotmail.com>
2025-03-25 10:49:46 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  function randomExcuse() {
+    let who = [ 'Mi perro', 'Mi gato', 'Mi mapache', 'Mi panda'];
+    let action = ['rompio', 'tiro', 'robo' ];
+    let what = ['mi telefono', 'mi ordenador', 'mi coche'];
+    let when = ['mientras estudiaba', 'mientras estaba en el gimnasio', 'mientras compraba'];
+  
+    const randomWho = Math.floor(Math.random() * who.length);
+    const randomAction = Math.floor(Math.random() * action.length);
+    const randomWhat = Math.floor(Math.random() * what.length);
+    const randomWhen = Math.floor(Math.random() * when.length);
+  
+    return `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+   }
+  
+  window.onload = function() {
+    const excuseElement = document.getElementById('excuse')
+    excuseElement.innerHTML = randomExcuse();
+  };
+ 
+
